51017c3b27036d872ae227a69c31545c1e68fcef
kimdohwan7220 <dohwan8220@naver.com>
2025-12-05 11:48:19 +0900

    feat : 8. PostDto 도입으로 Entitiy 숨김

---
+++ src/main/java/com/back/domain/post/post/dto/PostDto.java
@@ -0,0 +1,23 @@
+package com.back.domain.post.post.dto;
+
+import com.back.domain.post.post.entity.Post;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class PostDto {
+    private int id;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+    private String subject;
+    private String body;
+
+    public PostDto(Post post) {
+        this.id = post.getId();
+        this.createdDate = post.getCreateDate();
+        this.modifiedDate = post.getModifyDate();
+        this.subject = post.getTitle();
+        this.body = post.getContent();
+    }
+}
