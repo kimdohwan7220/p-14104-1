1826feb2e3e66f70bcbda4c701e7848e248d3e50
kimdohwan7220 <dohwan8220@naver.com>
2025-12-08 15:27:05 +0900

    refactor : 38. 테스트 케이스 수정

--- src/main/java/com/back/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/back/domain/post/post/repository/PostRepository.java
@@ -1,7 +1,9 @@
 package com.back.domain.post.post.repository;
 
 import com.back.domain.post.post.entity.Post;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    Optional<Post> findFirstByOrderByIdDesc();
 }
